9a176e59e9c909e226e761939fe5e8fe97aeb06e
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 17:49:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
     index = input_to_index(input)
 
     if valid_move?(index) == true
-      move(index, current_player(board))
+      move(index, current_player(@board))
       display_board
     else
       turn
